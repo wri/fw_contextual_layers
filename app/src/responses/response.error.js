5031b8a9b8996b3f1722756120d4630531b8d98e
Sam Davidge <sam@3sidedcube.com>
2021-11-24 09:51:05 +0000

    Ensure testsuite runs

--- app/src/responses/response.error.js
+++ app/src/responses/response.error.js
@@ -1,14 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ResponseError = void 0;
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.ResponseError = undefined;
 class ResponseError extends Error {
+
     constructor(statusCode, body, response) {
         super();
         this.name = 'ResponseError';
         this.statusCode = statusCode;
-        this.message = statusCode + ' - ' + (JSON && JSON.stringify ? JSON.stringify(body) : body);
+        this.message = `${statusCode} - ${JSON && JSON.stringify ? JSON.stringify(body) : body}`;
         this.error = body;
         this.response = response;
     }
+
 }
 exports.ResponseError = ResponseError;
