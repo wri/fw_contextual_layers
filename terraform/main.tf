9105b69335eedf0aaaa156763f164fd9d01eef06
Edward Martin <edward@3sidedcube.com>
2022-11-24 15:11:52 +0000

    tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,7 +51,7 @@ module "fargate_autoscaling" {
   lb_target_group_arn = module.fargate_autoscaling.lb_target_group_arn
   listener_arn        = data.terraform_remote_state.fw_core.outputs.lb_listener_arn
   project_prefix      = var.project_prefix
-  path_pattern        = ["/v1/fw_contextual_layers/healthcheck", "/v1/contextual-layer*"]
+  path_pattern        = ["/v1/fw_contextual_layers/healthcheck", "/v1/contextual-layer*", "/v3/contextual-layer*"]
   priority            = 5
 
   health_check_path = "/v1/fw_contextual_layers/healthcheck"
