2497196f628413954daa16c2d2c2ed045d5dfaf4
Burhanuddin Salim <burhan.salman@gmail.com>
2022-11-29 12:26:47 +0400

    Revert "Remove logging for healthcheck"
    This reverts commit fedd3d9f35bded2f24640bbea8713ad0056f87e4.

--- app/src/app.js
+++ app/src/app.js
@@ -93,12 +93,7 @@ app.use(async (ctx, next) => {
   }
 });
 
-app.use(function (req, res, next) {
-  if (req.originalUrl === '/v1/fw_contextual_layers/healthcheck') {
-    return next();
-  }
-  return koaLogger();
-});
+app.use(koaLogger());
 
 app.use(async (ctx, next) => {
   await loggedInUserService.setLoggedInUser(ctx, logger);
