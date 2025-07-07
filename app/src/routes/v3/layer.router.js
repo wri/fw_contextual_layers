d8556baac69301cd2d18aaaae518e606775d3c06
Edward Martin <edward@3sidedcube.com>
2025-07-07 13:32:09 +0100

    upgrades

--- app/src/routes/v3/layer.router.js
+++ app/src/routes/v3/layer.router.js
@@ -41,9 +41,7 @@ class Layer {
       ctx.throw(500, "Error while retrieving user team");
     }
 
-    const teamLayers = teams
-      .map((team) => team.layers ?? [])
-      .reduce((acc, layers) => [...acc, ...layers], []);
+    const teamLayers = teams.flatMap(team => team.layers ?? []);
 
     const query = {
       $and: [
